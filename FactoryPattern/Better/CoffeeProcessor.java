c1baaa4ef4082a75ff3717f2f2fd58b7062aaf15
syam-cpu <meghasyam2002@gmail.com>
2026-05-28 00:42:19 +0530

    add more parameters to coffee factory

--- FactoryPattern/Better/CoffeeProcessor.java
+++ FactoryPattern/Better/CoffeeProcessor.java
@@ -4,9 +4,9 @@ package FactoryPattern.Better;
 
 public class CoffeeProcessor {
 
-    public Coffee prepareCoffee(String whichCoffee)
+    public Coffee prepareCoffee(String whichCoffee, String whichMilk, String whichSugar, String whichCoffeeType)
     {
-        Coffee coffee = new CoffeeFactory().getCoffee(whichCoffee);
+        Coffee coffee = new CoffeeFactory().getCoffee(whichCoffee,whichMilk, whichSugar, whichCoffeeType);
         return coffee;
     }
 }
